c30c269b49fa0991260a26f520d3294bd7da11c5
Shivam Nagar <shivamnagar228@gmail.com>
2022-12-21 22:59:29 +0530

    first commit

---
+++ app/Http/Controllers/DemoController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DemoController extends Controller
+{
+    Public function writeMsg(){
+        echo "Hello World";
+    }
+}
